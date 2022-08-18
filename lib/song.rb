d6adda643599d3cc90cecd6d805b2866713d6045
michael muliro <michael.muliro@student.moringaschool.com>
2022-08-18 09:59:20 +0300

    fixing errors

--- lib/song.rb
+++ lib/song.rb
@@ -6,47 +6,47 @@ class Song
     @@genres = []
   
     def initialize(name, artist, genre)
-      @name = name
-      @artist = artist
-      @genre = genre
-      @@count += 1
-      @@genres << genre
-      @@artists << artist
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
     end
   
     def self.count
-      @@count
+        @@count
     end
   
     def self.artists
-      @@artists.uniq
+        @@artists.uniq
     end
   
     def self.artist_count
-      artist_count = {}
-      @@artists.each do |artist|
-        if artist_count[artist]
-          artist_count[artist] += 1 
-        else
-          artist_count[artist] = 1
-        end
-      end
-      artist_count
+        artist_count = {}
+        @@artists.each do |artist|
+            if artist_count[artist]
+                artist_count[artist] += 1 
+            else
+                artist_count[artist] = 1
+            end
+            end
+        artist_count
     end
   
     def self.genres
-      @@genres.uniq
+        @@genres.uniq
     end
   
     def self.genre_count
-      genre_count = {}
-      @@genres.each do |genre|
-        if genre_count[genre]
-          genre_count[genre] += 1 
-        else
-          genre_count[genre] = 1
+        genre_count = {}
+        @@genres.each do |genre|
+            if genre_count[genre]
+                genre_count[genre] += 1 
+            else
+                genre_count[genre] = 1
+            end
         end
-      end
-      genre_count
+        genre_count
     end
   end
